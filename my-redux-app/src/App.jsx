2f7c149e3dd67e711a6e7c5929ac56f56291dab3
juan salas <juanjo.salas.dev@gmail.com>
2024-06-05 13:07:32 +0200

    Home

--- my-redux-app/src/App.jsx
+++ my-redux-app/src/App.jsx
@@ -2,6 +2,7 @@ import { React, useState } from 'react'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from './components/Register/Register';
+import Home from "./components/Home/Home";
 import './App.scss'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
     <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} /> 
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
