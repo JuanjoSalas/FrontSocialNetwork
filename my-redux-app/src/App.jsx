994bdca244dc74408424e1f73c0b8e741acbba7a
juan salas <juanjo.salas.dev@gmail.com>
2024-06-05 13:42:49 +0200

    foot addedd

--- my-redux-app/src/App.jsx
+++ my-redux-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from './components/Register/Register';
 import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
 import Home from "./components/Home/Home";
 import './App.scss';
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/search/:userName" element={<Search />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   )
