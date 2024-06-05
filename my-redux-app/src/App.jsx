d883aed1744805dfbf071738bbfd76c2cbb05196
juan salas <juanjo.salas.dev@gmail.com>
2024-06-05 13:17:35 +0200

    Header

--- my-redux-app/src/App.jsx
+++ my-redux-app/src/App.jsx
@@ -2,13 +2,15 @@ import { React, useState } from 'react'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from './components/Register/Register';
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import './App.scss'
+import './App.scss';
 
 function App() {
   return (
     <>
     <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="/login" element={<Login />} />
