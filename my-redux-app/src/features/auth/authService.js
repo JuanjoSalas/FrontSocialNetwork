6d0faf66770c66fc461d292875ca975a30d8e59a
AgusCorrales <aguscorrales1997@gmail.com>
2024-06-05 13:17:32 +0200

    Search

--- my-redux-app/src/features/auth/authService.js
+++ my-redux-app/src/features/auth/authService.js
@@ -9,7 +9,7 @@ const res = await axios.post(API_URL , userData);
 };
 
 const getUserByUserName = async (username)=>{
-    const res = await axios.get(API_URL + "name" + username)
+    const res = await axios.get(API_URL + "/name", username)
     return res.data
 }
 
