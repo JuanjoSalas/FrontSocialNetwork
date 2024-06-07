6fd9858876b37100b3aef2839cc23579064a641b
AgusCorrales <aguscorrales1997@gmail.com>
2024-06-07 14:05:48 +0200

    Scss Profile and UserDetail

--- my-redux-app/src/components/Header/Header.jsx
+++ my-redux-app/src/components/Header/Header.jsx
@@ -1,51 +1,65 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { logout } from "../../features/auth/authSlice"
+import { logout } from "../../features/auth/authSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faSignOutAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
+import "./Header.scss";
 
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
   const [search, setSearch] = useState("");
   
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
-    if (e.key == "Enter") {
-      navigate("/search/"+ search)
+    if (e.key === "Enter") {
+      navigate("/search/" + search);
     }
   };
   
   return (
-    <div>
-      <span>
-        <Link to="/">Home /</Link>
-      </span>
-      <input type="text" name="search" onKeyUp={handleSearch} />
-      {user ? (
-        <>
-        <span>
-          <Link to="/profile">Profile /</Link>
-        </span>
-          <span
-            onClick={() => {
-              dispatch(logout());
-            }}
-          >
-            <Link to="/login"> Logout</Link>
-          </span>
-        </>
-      ) : (
-        <>
-          <span>
-            <Link to="/login"> Login /</Link>
-          </span>
-          <span>
-            <Link to="/register"> Register /</Link>
-          </span>
-        </>
-      )}
+    <div className="header">
+      <div className="header-left">
+        <Link to="/" className="header-icon">
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
+        {user && (
+          <input 
+            type="text" 
+            name="search" 
+            placeholder="Search" 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyUp={handleSearch} 
+            className="header-search"
+          />
+        )}
+      </div>
+      <div className="header-right">
+        {user ? (
+          <>
+            <Link to="/profile" className="header-icon">
+              <FontAwesomeIcon icon={faUser} />
+            </Link>
+            <span
+              className="header-icon logout-icon"
+              onClick={() => {
+                dispatch(logout());
+              }}
+            >
+              <FontAwesomeIcon icon={faSignOutAlt} />
+            </span>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="header-link">Login</Link>
+            <Link to="/register" className="header-link">Register</Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- my-redux-app/src/components/UserDetail/UserDetail.jsx
+++ my-redux-app/src/components/UserDetail/UserDetail.jsx
@@ -1,28 +1,41 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getUserById } from '../../features/auth/authSlice';
-import {  Wrap, WrapItem, Avatar } from '@chakra-ui/react'
+import { Card } from 'antd';
+import { Image } from '@chakra-ui/react';
+
+const { Meta } = Card;
 
 const UserDetail = () => {
-    const {id} = useParams();
-    const {user} = useSelector((state)=> state.auth);
-    const dispatch = useDispatch();
+  const { id } = useParams();
+  const { user } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getUserById(id));
-      }, [id]);
-      if(!user) return <p>cargando...</p>
+  useEffect(() => {
+    dispatch(getUserById(id));
+  }, [id]);
 
-    return (
+  if (!user) return <p>cargando...</p>;
+
+  return (
     <div>
       <h3>UserDetail</h3>
-      <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />  
-      <p>Nombre: {user.firstname}</p>
-      <p>Apellido: {user.lastname}</p>
-      <p>Usuario: {user.username}</p>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Image src='https://bit.ly/dan-abramov' borderRadius='full' boxSize='150px' />
+        <div style={{ marginLeft: 20 }}>
+          <Card
+            title='User Details'
+            style={{ width: 300 }}
+          >
+            <Meta title={`Nombre: ${user.firstname}`} />
+            <Meta title={`Apellido: ${user.lastname}`} />
+            <Meta title={`Usuario: ${user.username}`} />
+          </Card>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default UserDetail;
