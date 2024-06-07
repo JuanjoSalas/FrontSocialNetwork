f7d825113183fe7c87d566276d6a2387ca6b64cd
juan salas <juanjo.salas.dev@gmail.com>
2024-06-07 12:00:16 +0200

    postDetail

--- my-redux-app/src/components/PostDetail/PostDetail.jsx
+++ my-redux-app/src/components/PostDetail/PostDetail.jsx
@@ -1,9 +1,23 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch  } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getPostById } from "../../features/posts/postsSlice";
 
 const PostDetail = () => {
     const { _id } = useParams();
-    console.log(_id);
-    return <div>PostDetail</div>;
+    const dispatch = useDispatch();
+    const { post } = useSelector((state) => state.posts);
+    useEffect(() => {
+        dispatch(getPostById(_id));
+    }, []);
+
+    return ( 
+    <div>
+    <h1>PostDetail</h1>
+    <p>{post.tittle}</p>
+    <p>{post.body}</p>
+    </div>
+    );
 };
 
 export default PostDetail;
