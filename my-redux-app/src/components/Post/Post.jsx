1765380f07e1f9b8560a8d75a794c438fd80d006
juan salas <juanjo.salas.dev@gmail.com>
2024-06-12 10:27:58 +0200

    link fix

--- my-redux-app/src/components/Post/Post.jsx
+++ my-redux-app/src/components/Post/Post.jsx
@@ -10,7 +10,7 @@ const Post = () => {
             <div className='container-post'>
                 <div className='border-post'>
                     <div className="post" key={post._id}>
-                        <Link to={"/post/" + post._id}>                    
+                        <Link className="link" to={"/post/" + post._id}>                    
                             <p className="detail-post">{post.title}</p>
                             <img className="image" src="https://images.stockcake.com/public/2/6/a/26a3fd95-08ee-4b93-b506-d6dfc85c0414_large/mountain-biking-adventure-stockcake.jpg" alt="" />
                             <p className="detail-post">{post.body}</p>
