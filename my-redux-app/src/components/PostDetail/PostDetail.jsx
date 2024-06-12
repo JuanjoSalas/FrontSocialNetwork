bf27b1adeceb31acfaaf01d7772037b5cb4add43
juan salas <juanjo.salas.dev@gmail.com>
2024-06-12 13:00:27 +0200

    reponsive

--- my-redux-app/src/components/PostDetail/PostDetail.jsx
+++ my-redux-app/src/components/PostDetail/PostDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch  } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPostById } from "../../features/posts/postsSlice";
-
+import "./PostDetail.scss";
 
 const PostDetail = () => {
     const { id } = useParams();
@@ -14,12 +14,15 @@ const PostDetail = () => {
     }, [id]);
 
     return ( 
-    <div>
-    <h1>PostDetail</h1>
-    <p>Titulo: {post.title}</p> 
-    <p>Body: {post.body}</p>
-    </div>
-    
+        <div className='container-postDetail'>
+            <div className='border-postDetail'>
+                <div className="postDetail">
+                    <p className="detail-post">{post.title}</p>
+                    <img className="postImage" src="https://images.stockcake.com/public/2/6/a/26a3fd95-08ee-4b93-b506-d6dfc85c0414_large/mountain-biking-adventure-stockcake.jpg" alt="" />
+                    <p className="postBody">{post.body}</p>
+                </div>
+            </div>
+        </div>
     );
 };
 
