a841c981f20a4b9cd58f4ef71e11218c66b9570c
juan salas <juanjo.salas.dev@gmail.com>
2024-06-13 12:59:01 +0200

    commennts print

--- my-redux-app/src/components/PostDetail/PostDetail.jsx
+++ my-redux-app/src/components/PostDetail/PostDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch  } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { getPostById } from "../../features/posts/postsSlice";
+import Comment from "../Comment/Comment";
 import "./PostDetail.scss";
 
 const PostDetail = () => {
@@ -20,6 +21,7 @@ const PostDetail = () => {
                     <p className="detail-post">{post.title}</p>
                     <img className="postImage" src="https://images.stockcake.com/public/2/6/a/26a3fd95-08ee-4b93-b506-d6dfc85c0414_large/mountain-biking-adventure-stockcake.jpg" alt="" />
                     <p className="postBody">{post.body}</p>
+                    <div className="container-comment"><div className="comment-unique"><Comment /></div></div>
                     <Link className="add-comment" to="/comments"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAASdJREFUSEvtltERgjAMhpNJxE1kE51EmUSdREbBSaI/13IVWtrSQn2gdzxQIF+T/AlhKrS4EJcmYBGpiOic+UAdMz9Mmz9gEXkR0SkzVJsD/KhvBrCIwMv7SlBttmHmG25MMDau/w7uiOiiDonDhqYn2eOWmWuAI3Wxg/uQxYhrk1CbQjJrEvs4sE1YLsFF5XjwLrTUZgQXB/7WezOGMnOrPEaLxWUuNKLxHp5HgW2ObpLjHYwIlAu1ITiXkGwpShZXaGWN37OC0QgwCKy5al2G5ScQ7aaaRMziP3hmMMxSb0+Y5mcu28eqF9tS0PdwHbrY/HjHWwd4EEkscDLsuQyMwPASAun/TCkr1GPU6lNPiCnAGI+rHB5O/m45Tr/EhjfUS4yGfPMBdTzDHy5taC4AAAAASUVORK5CYII="/></Link>
                 </div>
             </div>
