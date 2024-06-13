351ccc840142fdb29966838d4b05f19e660abcd4
juan salas <juanjo.salas.dev@gmail.com>
2024-06-13 12:15:25 +0200

    profile evolution

--- my-redux-app/src/components/Profile/Profile.jsx
+++ my-redux-app/src/components/Profile/Profile.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserInfo } from '../../features/auth/authSlice';
 import { useParams } from 'react-router-dom';
-import wallpaper from "../../assets/img/wallpaper.png"
+import wallpaper from "../../assets/img/wallpaper.png";
+import Comment from "../Comment/Comment";S
 import "./Profile.scss"
 
 const formatBirthday = (isoDate) => {
@@ -35,12 +36,16 @@ const Profile = () => {
           <div className="card__subtitle">{user.username}</div>
           <div className="card__subtitle">{user.email}</div>
           <div className="card__subtitle">{user.birthday ? formatBirthday(user.birthday) : ''}</div>
+          <p className='icon-posts'><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAOhJREFUSEvtltENgzAMRH2btJuUTcomHYVOUjYpm1xlRKQQShIFtxFS8hvHz2cncJBKC5W4sgsmeReRi0FhI4AxzLMBk1TYywjqeAOA3od/Ayv0ZqDUTzEBuKbAXAK0Rd2RAkiqABWiq/NbvlIcCywpwARM8rHAtSOp1zBfKCuwG0GO+Lmt5wbnyAxjTBQ3cE4HWqvP9eXKmWl7TqW3uqS7sTPZv8Vq4ElEnoZ0tT+ac14xI9ADGAzBq1QhWI3ee4nYGDTLIv5l9uKeS3d/ZG/TYA/uLK6Fqde0+5fLcoapXCm3mDpfvP8BmR/fH6KPn9UAAAAASUVORK5CYII="/></p>
           {user.PostIds && user.PostIds.length > 0 ? (
             user.PostIds.map(post => (
+              <div className='container-profile-post'>
               <div key={post._id}>
-                <h3>{post.title}</h3>
+                <p className="detail-post"> {post.title}</p>
                 <img className="image" src="https://images.stockcake.com/public/2/6/a/26a3fd95-08ee-4b93-b506-d6dfc85c0414_large/mountain-biking-adventure-stockcake.jpg" alt="" />
-                <p>{post.body}</p>
+                <p className="body">{post.body}</p>
+                <p>{Comment}</p>
+              </div>
               </div>
             ))
           ) : (
